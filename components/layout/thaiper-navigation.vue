4967c87d66f8b929c56751360055a1452f9ef4d7
gbezyuk <dev@gbezyuk.ru>
2022-03-18 22:11:25 +0300

    Khmer Lao Hindi Tamil Sinhala

--- components/layout/thaiper-navigation.vue
+++ components/layout/thaiper-navigation.vue
@@ -3,13 +3,25 @@
     <nuxt-link to="/" exact>Home</nuxt-link>
     <span>
         <span>Keyboards:</span>
+
         <nuxt-link to="/keyboards/english" exact>English</nuxt-link>
+
         <nuxt-link to="/keyboards/russian" exact>Russian</nuxt-link>
-        <nuxt-link to="/keyboards/thai" exact>Thai</nuxt-link>
+
         <nuxt-link to="/keyboards/hebrew" exact>Hebrew</nuxt-link>
+
         <nuxt-link to="/keyboards/georgian" exact>Georgian</nuxt-link>
         <nuxt-link to="/keyboards/armenian" exact>Armenian</nuxt-link>
         <nuxt-link to="/keyboards/greek" exact>Greek</nuxt-link>
+
+        <nuxt-link to="/keyboards/thai" exact>Thai</nuxt-link>
+        <nuxt-link to="/keyboards/khmer" exact>Khmer</nuxt-link>
+        <nuxt-link to="/keyboards/lao" exact>Lao</nuxt-link>
+
+        <nuxt-link to="/keyboards/hindi" exact>Hindi</nuxt-link>
+        <nuxt-link to="/keyboards/tamil" exact>Tamil</nuxt-link>
+        <nuxt-link to="/keyboards/sinhala" exact>Sinhala</nuxt-link>
+
     </span>
 </nav>
 </template>
---
+++ pages/keyboards/khmer.vue
@@ -0,0 +1,19 @@
+<template>
+    <dl-keyboard :map="layout"/>
+</template>
+
+<script>
+import DlKeyboard from '~/components/dl-keyboard/dl-keyboard'
+import layout from '~/assets/langpacks/khmer/keyboard_layouts/macos.json'
+
+export default {
+    components: {
+        DlKeyboard
+    },
+    computed: {
+        layout () {
+            return layout
+        }
+    }
+}
+</script>
