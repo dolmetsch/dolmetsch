1bd7641b1c98e3d286599dde00f1edd6ef66c0e2
gbezyuk <dev@gbezyuk.ru>
2022-03-19 17:03:10 +0300

    hebrew word list added

---
+++ pages/_language/word-list.vue
@@ -0,0 +1,28 @@
+<template>
+<dl>
+    <template v-for="(v, k) in words">
+        <dt>
+            <strong>{{ k }}</strong>
+            {{ v }}
+
+        </dt>
+        <dd><audio :src="`/langpacks/hebrew/audio/${k}.mp3`" controls></audio></dd>
+    </template>
+</dl>
+</template>
+
+<script>
+import words from '~/assets/langpacks/hebrew/hebrew2english.json'
+
+export default {
+    computed: {
+        words () {
+            return words
+        },
+    }
+}
+
+</script>
+
+<style lang="stylus" scoped>
+</style>
